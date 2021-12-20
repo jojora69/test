3cfc19611c4d0de6a301c74c8ddcef1e9bb9c08a
jojora69 <79311460+jojora69@users.noreply.github.com>
2021-12-20 16:32:44 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿
+namespace test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.telBox = new System.Windows.Forms.TextBox();
+            this.adresBox = new System.Windows.Forms.TextBox();
+            this.add_button = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.selectbutton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.add_button);
+            this.tabPage1.Controls.Add(this.adresBox);
+            this.tabPage1.Controls.Add(this.telBox);
+            this.tabPage1.Controls.Add(this.nameBox);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 424);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Insert";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.comboBox1);
+            this.tabPage2.Controls.Add(this.searchBox);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.selectbutton);
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 424);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Select";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(8, 40);
+            this.nameBox.Multiline = true;
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(184, 33);
+            this.nameBox.TabIndex = 0;
+            // 
+            // telBox
+            // 
+            this.telBox.Location = new System.Drawing.Point(8, 150);
+            this.telBox.Multiline = true;
+            this.telBox.Name = "telBox";
+            this.telBox.Size = new System.Drawing.Size(184, 33);
+            this.telBox.TabIndex = 1;
+            // 
+            // adresBox
+            // 
+            this.adresBox.Location = new System.Drawing.Point(8, 96);
+            this.adresBox.Multiline = true;
+            this.adresBox.Name = "adresBox";
+            this.adresBox.Size = new System.Drawing.Size(184, 33);
+            this.adresBox.TabIndex = 2;
+            // 
+            // add_button
+            // 
+            this.add_button.Location = new System.Drawing.Point(8, 247);
+            this.add_button.Name = "add_button";
+            this.add_button.Size = new System.Drawing.Size(144, 41);
+            this.add_button.TabIndex = 3;
+            this.add_button.Text = "Добавить";
+            this.add_button.UseVisualStyleBackColor = true;
+            this.add_button.Click += new System.EventHandler(this.add_button_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(786, 203);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // selectbutton
+            // 
+            this.selectbutton.Location = new System.Drawing.Point(6, 387);
+            this.selectbutton.Name = "selectbutton";
+            this.selectbutton.Size = new System.Drawing.Size(121, 29);
+            this.selectbutton.TabIndex = 1;
+            this.selectbutton.Text = "Вывод";
+            this.selectbutton.UseVisualStyleBackColor = true;
+            this.selectbutton.Click += new System.EventHandler(this.selectbutton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(150, 387);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(634, 29);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(150, 364);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Запрос";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 227);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Поиск";
+            // 
+            // searchBox
+            // 
+            this.searchBox.Location = new System.Drawing.Point(52, 227);
+            this.searchBox.Multiline = true;
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(175, 27);
+            this.searchBox.TabIndex = 5;
+            this.searchBox.TextChanged += new System.EventHandler(this.searchBox_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "New York",
+            "Москва ",
+            "California",
+            "Все"});
+            this.comboBox1.Location = new System.Drawing.Point(622, 227);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(162, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(619, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Фильтр";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button add_button;
+        private System.Windows.Forms.TextBox adresBox;
+        private System.Windows.Forms.TextBox telBox;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.Button selectbutton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["testDB"].ConnectionString);
+
+            conn.Open();
+
+            if (conn.State == ConnectionState.Open)
+            {
+                MessageBox.Show("+");
+            }
+
+            conn.Close();
+        }
+
+        private void add_button_Click(object sender, EventArgs e)
+        {
+            string query = $"INSERT INTO sotrudniki ( name, address, tel) VALUES (N'{nameBox.Text}', N'{adresBox.Text}', N'{telBox.Text}')";
+            MySqlConnection conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["testDB"].ConnectionString);
+            MySqlCommand cmDB = new MySqlCommand( query, conn);
+            try
+            {
+                conn.Open();
+                MySqlDataReader rd = cmDB.ExecuteReader();
+                conn.Close();
+                MessageBox.Show("Успешно");
+                nameBox.Clear();
+                adresBox.Clear();
+                telBox.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Неудачно");
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void selectbutton_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["testDB"].ConnectionString);
+            MySqlDataAdapter msad = new MySqlDataAdapter("select * from sotrudniki", conn);
+
+            DataSet dataSet = new DataSet();
+
+            msad.Fill(dataSet);
+
+            dataGridView1.DataSource = dataSet.Tables[0];
+        }
+
+        private void searchBox_TextChanged(object sender, EventArgs e)
+        {
+            (dataGridView1.DataSource as DataTable).DefaultView.RowFilter = $"name LIKE '%{searchBox.Text}%'";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+
+                    (dataGridView1.DataSource as DataTable).DefaultView.RowFilter = $"address = 'New York'";
+
+                    break;
+
+                case 1:
+
+                    (dataGridView1.DataSource as DataTable).DefaultView.RowFilter = $"address = 'Москва'";
+
+                    break;
+
+                case 2:
+
+                    (dataGridView1.DataSource as DataTable).DefaultView.RowFilter = $"address = 'California'";
+
+                    break;
+
+                case 3:
+
+                    (dataGridView1.DataSource as DataTable).DefaultView.RowFilter = "";
+
+                    break;
+            }
+        }
+    }
+}
